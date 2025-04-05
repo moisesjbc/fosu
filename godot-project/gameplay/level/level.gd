0eac38d14bf33db7872b81c20253e91b0a8a1906
Moisés J <moisesjbc@gmail.com>
2025-04-05 15:24:47 +0100

    Add driller movement along with row

--- godot-project/gameplay/level/level.gd
+++ godot-project/gameplay/level/level.gd
@@ -7,6 +7,11 @@ const RIGHT_ARROW_ID = 2
 const FIRST_TILE_COLUMN = 1
 const LAST_TILE_COLUMN = 6
 
+const TILE_WIDTH = 128
+const TILES_PER_ROW = 6
+
+var driller
+
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed == false:
@@ -25,6 +30,14 @@ func move_row_left(row_index):
 		$tilemap.set_cell(column_index, row_index, $tilemap.get_cell(column_index + 1, row_index))
 
 	$tilemap.set_cell(LAST_TILE_COLUMN, row_index, first_tile)
+	
+	# Move driller (if in the row)
+	var driller_pos = $tilemap.world_to_map(driller.global_position)
+	if driller_pos.y == row_index:
+		if driller_pos.x > FIRST_TILE_COLUMN:
+			driller.translate(Vector2(-TILE_WIDTH, 0))
+		else:
+			driller.translate(Vector2(TILE_WIDTH * (TILES_PER_ROW - 1), 0))
 
 
 func move_row_right(row_index):
@@ -36,9 +49,16 @@ func move_row_right(row_index):
 		column_index -= 1
 
 	$tilemap.set_cell(FIRST_TILE_COLUMN, row_index, last_tile)
+	
+	# Move driller (if in the row)
+	var driller_pos = $tilemap.world_to_map(driller.global_position)
+	if driller_pos.y == row_index:
+		if driller_pos.x < LAST_TILE_COLUMN:
+			driller.translate(Vector2(+TILE_WIDTH, 0))
+		else:
+			driller.translate(Vector2(-TILE_WIDTH * (TILES_PER_ROW - 1), 0))
 
 
 func _on_driller_block_drilled(driller):
 	var tilemap_pos = $tilemap.world_to_map(driller.global_position)
 	$tilemap.set_cellv(tilemap_pos, -1)
-	print("Driller at tile ", tilemap_pos)
--- godot-project/gameplay/main/main.gd
+++ godot-project/gameplay/main/main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+func _ready():
+	$level.driller = $driller
 
 
 func _on_target_area_driller_entered():
