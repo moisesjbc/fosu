398fec2d8c6e1c365ede5fa2874c4d08d954bead
Moisés J <moisesjbc@gmail.com>
2025-04-06 11:44:52 +0100

    Add styles and main menu button to game over screen

--- src/gameplay/gui/game_over/game_over.gd
+++ src/gameplay/gui/game_over/game_over.gd
@@ -9,3 +9,8 @@ func start():
 func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+
+func _on_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://gui/main_menu/main_menu.tscn")
