60dfb350b26defdb9c465aad399891edbed60bf5
Moisés J <moisesjbc@gmail.com>
2025-04-06 19:22:45 +0100

    Adjust sfx volume in mobile

--- src/globals/global_sound_system/global_sound_system.gd
+++ src/globals/global_sound_system/global_sound_system.gd
@@ -6,7 +6,8 @@ onready var on_mobile_platform = JavaScript.eval("/Android|webOS|iPhone|iPad|iPo
 
 func _ready():
 	if on_mobile_platform:
-		$button_pressed.volume_db = -20
+		var sfx_index = AudioServer.get_bus_index("sfx")
+		AudioServer.set_bus_volume_db(sfx_index, -20)
 
 
 func play_button_pressed_sound():
